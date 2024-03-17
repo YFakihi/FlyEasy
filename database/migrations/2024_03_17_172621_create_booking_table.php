6649db53c21ab963b02d2e256e442d00d2c65754
yfakihi <fakihiyassin01@gmail.com>
2024-03-17 17:41:14 +0000

    initial commit

---
+++ database/migrations/2024_03_17_172621_create_booking_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('airport_id')->constrained()->onDelete('cascade');
+            $table->foreignId('fast_track_service_id')->constrained()->onDelete('cascade');
+            $table->date('date');
+            $table->time('time');
+            $table->enum('status', ['pending', 'confirmed', 'canceled']);
+            $table->enum('service_type', ['departure_fast_track', 'arrival_fast_track']);
+            $table->integer('number_of_adults');
+            $table->integer('number_of_children');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking');
+    }
+};

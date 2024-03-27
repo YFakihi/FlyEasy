68fb939d17301c8aa0ecb47dea610498125481fe
yfakihi <fakihiyassin01@gmail.com>
2024-03-27 08:13:55 +0000

    inital commit

--- resources/views/dashboard/airports.blade.php
+++ resources/views/dashboard/airports.blade.php
@@ -219,127 +219,41 @@
     </div>
 </div>
 
-<script>
-    // delete functionality
-document.addEventListener('DOMContentLoaded', function () {
-    const deleteButtons = document.querySelectorAll('[data-modal-target="deleteModal"]');
-    const deleteModal = document.getElementById('deleteModal');
-    const deleteForm = document.getElementById('deleteForm');
-    const deleteRecordIdInput = document.getElementById('deleteRecordId');
-    const deleteConfirmButton = deleteModal.querySelector('.delete-confirm-button');
-
-    deleteButtons.forEach(function (button) {
-        button.addEventListener('click', function () {
-            const recordId = button.getAttribute('data-record-id');
-            const deleteAction = button.getAttribute('data-action');
-
-            deleteForm.action = deleteAction;
-            deleteRecordIdInput.value = recordId;
-
-            deleteModal.classList.remove('hidden');
-        });
-    });
-
-    deleteForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-
-        fetch(deleteForm.action, {
-            method: 'POST',
-            body: new FormData(deleteForm),
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.success) {
-                    console.log(data.message);
-
-                    const successMessage = document.getElementById('successMessage');
-                    if (successMessage) {
-                        successMessage.innerText = data.message;
-                    }
-
-                    location.reload();
-                } else {
-                    console.error(data.message);
-                }
-            })
-            .catch(error => {
-                console.error('Error deleting record:', error.message);
-            })
-            .finally(() => {
-                deleteModal.classList.add('hidden');
-            });
-    });
-});
-</script>
-
-
-
 <!-- Update modal -->
-<div id="updateairportModal{{ $airport->id }}" tabindex="-1"
-    aria-hidden="true"
-    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="updateairportModal{{ $airport->id }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-2xl max-h-full">
         <!-- Modal content -->
-        <div
-            class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+        <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
             <!-- Modal header -->
-            <div
-                class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
-                <h3
-                    class="text-lg font-semibold text-gray-900 dark:text-white">
-                    Update airport</h3>
-                <button type="button"
-                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-toggle="updateairportModal{{ $airport->id }}">
-                    <svg aria-hidden="true" class="w-5 h-5"
-                        fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd" />
+            <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    Update airport
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" onclick="closeModal('updateairportModal{{ $airport->id }}')">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
                     </svg>
                     <span class="sr-only">Close modal</span>
                 </button>
             </div>
             <!-- Modal body -->
-            <form action="{{route('airports.update',$airport->id)}}"
-                method="POST">
+            <form action="{{ route('airports.update', $airport->id) }}" method="POST">
                 @csrf
-                @if (isset($airport))
-                    @method('PUT')
-                @endif
-
-                <div class="grid gap-4 mb-4 sm:grid-cols-2">
+                @method('PUT')
+  <div class="grid gap-4 mb-4 sm:grid-cols-2">
                     <div>
-                        <label for="name"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <input type="text" name="name"
-                            id="name"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="Name"
-                            value="{{ isset($airport) ? $airport->name : '' }}"
-                            required="">
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Name" value="{{ $airport->name }}" required="">
                     </div>
                 </div>
 
-                <button type="submit"
-                    class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                            clip-rule="evenodd" />
+                <button type="submit" class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
                     </svg>
                     Update airport
                 </button>
             </form>
-
         </div>
     </div>
 </div>
@@ -444,5 +358,72 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
   @endsection
+  <script>
+    // delete functionality
+document.addEventListener('DOMContentLoaded', function () {
+    const deleteButtons = document.querySelectorAll('[data-modal-target="deleteModal"]');
+    const deleteModal = document.getElementById('deleteModal');
+    const deleteForm = document.getElementById('deleteForm');
+    const deleteRecordIdInput = document.getElementById('deleteRecordId');
+    const deleteConfirmButton = deleteModal.querySelector('.delete-confirm-button');
 
-  
+    deleteButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            const recordId = button.getAttribute('data-record-id');
+            const deleteAction = button.getAttribute('data-action');
+
+            deleteForm.action = deleteAction;
+            deleteRecordIdInput.value = recordId;
+
+            deleteModal.classList.remove('hidden');
+        });
+    });
+
+    deleteForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        fetch(deleteForm.action, {
+            method: 'POST',
+            body: new FormData(deleteForm),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.success) {
+                    console.log(data.message);
+
+                    const successMessage = document.getElementById('successMessage');
+                    if (successMessage) {
+                        successMessage.innerText = data.message;
+                    }
+
+                    location.reload();
+                } else {
+                    console.error(data.message);
+                }
+            })
+            .catch(error => {
+                console.error('Error deleting record:', error.message);
+            })
+            .finally(() => {
+                deleteModal.classList.add('hidden');
+            });
+    });
+});
+
+    function openModal(modalId) {
+        var modal = document.getElementById(modalId);
+        modal.classList.remove('hidden');
+        modal.setAttribute('aria-hidden', 'false');
+    }
+
+    function closeModal(modalId) {
+        var modal = document.getElementById(modalId);
+        modal.classList.add('hidden');
+        modal.setAttribute('aria-hidden', 'true');
+    }
+</script>

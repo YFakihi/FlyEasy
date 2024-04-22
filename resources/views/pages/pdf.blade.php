26df1fbd4716dc3e8df4b3931c05afaa2bf6761e
yfakihi <fakihiyassin01@gmail.com>
2024-04-22 23:37:20 +0100

    Fix broken link and update route name in home.blade.php

--- resources/views/pages/pdf.blade.php
+++ resources/views/pages/pdf.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Invoice</title>
-    <link rel="stylesheet" href="{{ asset('pdf.css') }}" type="text/css"> 
-</head>
+    <link rel="stylesheet" href="{{ public_path('pdf.css') }}" type="text/css"></head>
 <body>
     <table class="w-full">
         <tr>

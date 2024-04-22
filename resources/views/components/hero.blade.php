7422bb1b767e6490684422ebfbfe6e8319d00fb4
yfakihi <fakihiyassin01@gmail.com>
2024-04-22 14:36:02 +0100

    Update hero.blade.php to fix grid width and padding in background-slider section

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
 <section class="bg-white relative dark:bg-gray-900" id="background-slider"
 style="background: url('{{ asset("images/Marrakech-Car.jpg") }}');"  background-size: cover; background-repeat: no-repeat; background-position: center; padding-top: 120px; padding-bottom: 120px; position: relative;">
         <div class="bg-black opacity-50 absolute inset-0 z-0"></div> <!-- Overlay element -->
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 relative z-10">
+        <div class="grid w-full px-4 py-18 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 relative z-10">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1
                     class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">

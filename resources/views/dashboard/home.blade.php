fc015ddee74bf711cdf3d27fc3e6f9c05650fbbc
yfakihi <fakihiyassin01@gmail.com>
2024-04-22 14:36:34 +0100

    Update route name in home.blade.php to fix broken link

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -359,7 +359,7 @@
           </li>
           <li>
             <a
-              href="{{route('bookingTable')}}"
+              href="{{route('listbooking')}}"
               class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
 

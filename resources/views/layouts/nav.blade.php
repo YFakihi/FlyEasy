ebfc388d5e170b233364e6e255de5cf72130fa80
yfakihi <fakihiyassin01@gmail.com>
2024-04-22 14:36:45 +0100

    Fix broken links and update navigation in nav.blade.php

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -113,7 +113,7 @@
             <div class="grid grid-cols-3 gap-4 p-4">
             
               <a
-                href="#"
+                href="{{ route('welcome') }}"
                 class="block p-4 text-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 group">
             
 
@@ -125,14 +125,11 @@
                 viewBox="0 0 20 20">
                   <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
                 </svg>
-                
-
-
                 <div class="text-sm text-gray-900 dark:text-white"></div>
               </a>
         
               <a
-                href="#"
+                href="{{ route('about') }}"
                 class="block p-4 text-center rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 group">
 
                 <svg 

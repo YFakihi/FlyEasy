8264102229669dac6c753101c7eaa9a86c62c162
yfakihi <fakihiyassin01@gmail.com>
2024-04-23 17:11:30 +0100

    Update background slider styling in hero.blade.php

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-<section class="bg-white relative dark:bg-gray-900" id="background-slider"
+<section class="bg-white relative dark:bg-gray-900 w-full" id="background-slider"
 style="background: url('{{ asset("images/Marrakech-Car.jpg") }}');"  background-size: cover; background-repeat: no-repeat; background-position: center; padding-top: 120px; padding-bottom: 120px; position: relative;">
         <div class="bg-black opacity-50 absolute inset-0 z-0"></div> <!-- Overlay element -->
         <div class="grid w-full px-4 py-18 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 relative z-10">
@@ -34,7 +34,7 @@ style="background: url('{{ asset("images/Marrakech-Car.jpg") }}');"  background-
                 <h2 class="max-w-2xl mb-6 font-medium text-gray-200 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                     Une autre facon de voyager!</h2>
                     <a href="{{ route('booking') }}"
-                    class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     Booking Now</a>
 
               
